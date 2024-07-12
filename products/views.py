c1a71a828486a4dbb11ec0fa1d589628eecb2305
James Hyland <james.hyland@iubh.de>
2024-07-12 09:31:42 +0000

    Dev: add sort functionality for already filtered products.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -19,7 +21,10 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = product.annotate(lower_name=Lower('name') )
+                products = products.annotate(lower_name=Lower('name') )
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -40,8 +45,9 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            
+
     current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
