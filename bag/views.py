4ac42d63012eee73b8a641764d13f5bea7b41b2b
James Hyland <james.hyland@iubh.de>
2024-07-15 09:11:21 +0000

    Dev: add shopping bag contexts to project

--- bag/views.py
+++ bag/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_bag(request):
+    print("View Bag function called")  # Debug print statement
     return render(request, 'bag/bag.html')
--- sorted_supplies/settings.py
+++ sorted_supplies/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # allauth requires this option
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,6 +154,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIERY_PERCENTAGE = 10
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
