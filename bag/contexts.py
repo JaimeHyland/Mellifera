db2e72d7db51a97847c07bc7117084f78fbfe825
James Hyland <james.hyland@iubh.de>
2024-07-16 08:02:34 +0000

    Dev: add sizes to product_detail.html and  bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -19,7 +19,6 @@ def bag_contents(request):
             'product': product,
         })
 
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
