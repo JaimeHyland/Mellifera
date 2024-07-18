e2df76335ea4ce54a4bfa9b0c76391a2d59013b8
James Hyland <james.hyland@iubh.de>
2024-07-18 08:06:19 +0000

    Dev: add quantity control to bag page.

--- sorted_supplies/settings.py
+++ sorted_supplies/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIERY_PERCENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 
 # Default primary key field type
