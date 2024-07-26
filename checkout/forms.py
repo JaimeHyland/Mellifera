1a645e09642206f657dc657179b4ea3b8c82a2f7
James Hyland <james.hyland@iubh.de>
2024-07-26 12:43:29 +0000

    UX: improve checkout interface.

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,7 +4,9 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'county', 'country', 'postcode',)
+        fields = ('full_name', 'email', 'phone_number',
+        'street_address1', 'street_address2',
+        'town_or_city', 'county', 'country', 'postcode',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,18 +17,17 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street address line 1',
             'street_address2': 'Street address line 2',
             'town_or_city': 'Town or city',
-            'county': 'County',
-            'country': 'Country',
+            'county': 'County, state or municipality',
             'postcode': 'Postcode',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f"{placeholders[field]} *"
-            else:
-                placeholder = f"{placeholders[field]}"
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-
